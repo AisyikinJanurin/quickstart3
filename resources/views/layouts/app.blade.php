862520b549ad1ee3f63ef7e83634385c088987a6
Aisyikin <aisyikinjanurin@gmail.com>
2016-04-26 17:50:18 +0800

    Initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!-- resources/views/layouts/app.blade.php -->
+
+<!DOCTYPE html>
+
+<head>
+    <title>Laravel Quickstart - Basic </title>
+
+    <!-- CSS AND JavaScript -->
+    </head>
+
+<body>
+<div class="container">
+    <nav class="navbar navbar-default">
+        <!-- Navbar Contents -->
+    </nav>
+</div>
+
+@yield('content')
+</body>
+</html>
